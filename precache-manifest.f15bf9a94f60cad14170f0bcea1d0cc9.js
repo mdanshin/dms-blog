d58fafed05f7991c403af45c34fd9ae688062208
Mikhail Danshin <mdanshin@gmail.com>
2020-03-07 23:09:02 +0300

    Updates

---
+++ precache-manifest.f15bf9a94f60cad14170f0bcea1d0cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc40b2ab52a960c7f32b16abe35bd57c",
+    "url": "/dms-blog/index.html"
+  },
+  {
+    "revision": "155f4c1bd7e412746343",
+    "url": "/dms-blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9cf4aad7e6968a2a20f0",
+    "url": "/dms-blog/static/js/2.9d9ae6c9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dms-blog/static/js/2.9d9ae6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155f4c1bd7e412746343",
+    "url": "/dms-blog/static/js/main.f4b01e68.chunk.js"
+  },
+  {
+    "revision": "19ca351574a96344f219",
+    "url": "/dms-blog/static/js/runtime-main.944d9577.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dms-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
