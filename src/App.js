0a42a855cd07050d9089bb7058b25f50b4144934
Mikhail Danshin <mdanshin@gmail.com>
2020-03-07 23:27:41 +0300

    add Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
